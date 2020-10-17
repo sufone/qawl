f4637adafc11f2274739b3da4f25f685e3072bdb
Naved Islam <navedcoded@gmail.com>
2020-10-17 16:17:37 +0300

    workling prevention buttons

--- src/components/Toolbar.svelte
+++ src/components/Toolbar.svelte
@@ -6,10 +6,18 @@
 
 <div>
     <span>Current page: <input bind:value="{$pageLeft}"> </span>
-    {#if $pageLeft < 603}
+
+    <!-- To prevent button after max page -->
+    {#if $pageLeft < 603} 
         <button on:click={pageLeft.increment}>Next page</button>
     {:else}
         <button disabled>Next page</button>
     {/if}
-    <button on:click={pageLeft.decrement}>Previous page</button>
+
+    <!-- To prevent button before min page -->
+    {#if $pageLeft > 2}
+        <button on:click={pageLeft.decrement}>Previous page</button>
+    {:else}
+        <button disabled>Previous page</button>
+    {/if}
 </div>
