f7438279a5af6618b52443cc8349981a7a7cc430
Naved Islam <navedcoded@gmail.com>
2020-10-27 18:03:24 +0300

    check left/right page on the comp

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -1,14 +1,25 @@
 <script>
-    import {pageLeft} from '../stores/page.js'
-    
-    $: pageRight = $pageLeft - 1 //subtract negative number to prevent string conversions
+    import {inputPage} from '../stores/page.js'
+    console.log("inputpage from page.svelte: "+$inputPage)
+
+    let left, right
+    $: { //processing so the right page gets set on the right side, pun intended
+        if ($inputPage % 2 === 0) {
+            left = $inputPage
+            right = $inputPage - 1
+        } else {
+            left=  $inputPage + 1
+            right = $inputPage
+        }
+    }
+
 </script>
 
 <div>
 
-    <img alt="Page {$pageLeft} of the Madina mushaf" src="../public/mushaf/{$pageLeft}.png" />
-    <img alt="Page {pageRight} of the Madina mushaf" src="../public/mushaf/{pageRight}.png" />
-    
+    <img alt="Page {left} of the Madina mushaf" src="../public/mushaf/{left}.png" />
+    <img alt="Page {right} of the Madina mushaf" src="../public/mushaf/{right}.png" />
+
 </div>
 
 <style>
