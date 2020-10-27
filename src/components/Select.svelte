1280944217c4a1043cdf8ce37e2a49479f70b525
Naved Islam <navedcoded@gmail.com>
2020-10-27 13:57:10 +0300

    basic selecting with the select

--- src/components/Select.svelte
+++ src/components/Select.svelte
@@ -1,23 +1,32 @@
 <script>
 import surahs from '../surahs.js'
 import { onMount } from 'svelte';
+import {pageLeft} from '../stores/page.js'
 
+let page = parseInt($pageLeft)
 
 onMount(async () => { //so it's not run before elements are rendered
 	var ele = document.getElementById("surahSelect");
     for (let i = 0; i < surahs.length; i++) {
+		console.log(surahs[i]['pageGreen'])
+
         ele.innerHTML = ele.innerHTML +
             '<option value="' + surahs[i]['pageGreen'] + '">' +
             parseInt(i+1) + '. ' + surahs[i]['name'] + '</option>';
     }
 });
 
-function test() {
+function updatePage() {
+	var selectedSurah = parseInt(document.getElementById("surahSelect").value);
+	pageLeft.set(selectedSurah);
+}
 
-};
 </script>
 
-<select id="surahSelect" ></select>
+<!-- <select id="surahSelect" on:change={() => {console.log(this.value)}} ></select> -->
+
+<select id="surahSelect" on:change="{() => {updatePage()}}"></select>
+
 
 <style>
 
