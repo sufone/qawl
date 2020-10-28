73dc8b1c304111f619c1252bd811ef23709ac40f
Naved Islam <navedcoded@gmail.com>
2020-10-28 08:53:30 +0300

    Slider 👍🏽

--- src/components/NumberSlider.svelte
+++ src/components/NumberSlider.svelte
@@ -8,12 +8,20 @@
 	$: if ($inputPage) {
 		page = $inputPage
 	}
-	$: if (page > 0 && page < 605) {
+	$: if (page > 0 && page < 605) { //this is why $inputPage is not bound directly, need a bit of control
 		inputPage.set(page)
 	}
 
 	let page
 </script>
 
-	Current page: <input type="number" name="pageNumberInput" bind:value={page}
-		id="pageNumberInput" max="604" min="1" title="Directly go to page…">
+<input id="slider" type=range bind:value={page} min=1 max=604>
+
+Current page: <input type="number" name="pageNumberInput" bind:value={page}
+	id="pageNumberInput" max="604" min="1" title="Directly go to page…">
+
+<style>
+	#slider {
+		direction: rtl;
+	}
+</style>
