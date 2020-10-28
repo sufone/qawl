e3a0cf6a1af6c66b251c8a379e1c484a3f46d79d
Naved Islam <navedcoded@gmail.com>
2020-10-28 07:30:53 +0300

    100% ready

--- src/components/Select.svelte
+++ src/components/Select.svelte
@@ -1,30 +1,24 @@
 <script>
-	//TODO: Next steps to fix this
-	//instead of directly binding the select to the store, instead have an intermediary var
-	//select on:blur can set inputPage directly
-	//and a $: reactive script can check $inputPage against surahs.js, and set intermediatery var to be like that (so the select also changes with it)
 
 	import surahs from '../surahs.js'
 	import {inputPage} from '../stores/page.js'
 
-	let selected
-	let selectedSurah
-	$: { // only sets
-		console.log(selected)
+	let selectVal
+
+	$: if ($inputPage) {
+		console.log($inputPage)
 		for (let i = surahs.length - 1; i >= 0; i--) {
-			if (selected < surahs[i]['pageGreen']) {
+			if ($inputPage < surahs[i]['pageGreen']) {
 				console.log(surahs[i-1]['name'])
-				selectedSurah = surahs[i-1]['pageGreen']
+				selectVal = surahs[i-1]['pageGreen']
 			}
 		}
-		inputPage.set((selectedSurah))
-
 	}
 
-	</script>
+</script>
 
 <form id="selectForm" >
-	<select id="surahSelect" bind:value={$inputPage} >
+	<select id="surahSelect" bind:value={selectVal} on:change={() => {inputPage.set(selectVal)}} >
 		{#each surahs as surah}
 			<option value={surah.pageGreen}>
 				{surah.name}
