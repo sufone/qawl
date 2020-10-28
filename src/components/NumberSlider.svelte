22fac0b5e1266b23fe3e54b075403cf8c29b649b
Naved Islam <navedcoded@gmail.com>
2020-10-28 08:49:29 +0300

    working number input

---
+++ src/components/NumberSlider.svelte
@@ -0,0 +1,19 @@
+<script>
+	import {inputPage} from '../stores/page.js'
+
+	function blankField() {
+        document.getElementById("pageNumberInput").value = ''
+	}
+
+	$: if ($inputPage) {
+		page = $inputPage
+	}
+	$: if (page > 0 && page < 605) {
+		inputPage.set(page)
+	}
+
+	let page
+</script>
+
+	Current page: <input type="number" name="pageNumberInput" bind:value={page}
+		id="pageNumberInput" max="604" min="1" title="Directly go to page…">
--- src/components/Toolbar.svelte
+++ src/components/Toolbar.svelte
@@ -1,20 +1,11 @@
 <script>
-    import {inputPage} from '../stores/page.js'
     import Select from './Select.svelte'
     import Buttons from './Buttons.svelte'
-
-    function blankField() {
-        document.getElementById("pageNumberInput").value = ''
-    }
-
+    import NumberSlider from './NumberSlider.svelte'
 </script>
 
 <div>
-    <form id="pageForm" on:submit|preventDefault={() => {inputPage.set(document.getElementById("pageNumberInput")); blankField();}}>
-        Current page: <input type="number" name="pageNumberInput"
-            id="pageNumberInput" placeholder="{$inputPage}" max="604" min="1" title="Directly go to page…">
-    </form>
-
+    <NumberSlider/>
 
     <Buttons/>
 
