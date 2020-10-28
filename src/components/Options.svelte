a5a417cef6c57de0e6a05208baac6a133f7057fb
Naved Islam <navedcoded@gmail.com>
2020-10-28 10:34:40 +0300

    options to choose from!

--- src/components/Options.svelte
+++ src/components/Options.svelte
@@ -1,1 +1,12 @@
+<script>
+	import { settingsStore } from "../stores/settings"
+</script>
+
 <a href="#/">Back to Quran</a>
+
+<select bind:value={$settingsStore.hyperlinkProvider}>
+	<option disabled> - English - </option>
+	<option value="https://quran.com/">Quran.com</option>
+	<option disabled> - Arabic - </option>
+	<option value="https://tafsir.app/">Tafsir.app</option>
+</select>
