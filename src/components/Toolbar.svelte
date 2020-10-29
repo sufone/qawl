79d4947a29b9b16f168edaef8447caeadad1fd62
Naved Islam <navedcoded@gmail.com>
2020-10-29 16:15:09 +0300

    make it an overlay

--- src/components/Toolbar.svelte
+++ src/components/Toolbar.svelte
@@ -26,6 +26,25 @@
     div#toolbar-main {
         display: flex;
         flex-direction: column;
+
+        cursor: default;
+        display: flex;
+        justify-content: space-around;
+        width: 90%;
+        margin-left: 5%;
+        margin-bottom: 1.5%;
+        position: fixed;
+        bottom: 0;
+        height: 6%;
+        box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);
+        transition: opacity 300ms ease;
+
+        background:rgba(0, 0, 0, 0.5);
+        z-index: 2;
+        backdrop-filter: blur(5px);
+        padding: 5px;
+        border-radius: 4px;
+
     }
     div#toolbar-minor {
         display: flex;
