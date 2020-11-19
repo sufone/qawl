252e384d4a85f3a94cfd841e7e4a4e2d3e8eb57c
Naved Islam <navedcoded@gmail.com>
2020-11-19 22:17:45 +0200

    re-organize content

--- src/components/Options.svelte
+++ src/components/Options.svelte
@@ -1,20 +1,28 @@
 <script>
 	import { hyperlinkProvider } from "../stores/settings"
+
+	const appVersion = window.remote.require('electron').app.getVersion()
+
 </script>
 
-<p>Choose your online reference provider:</p>
+<div id="container">
 	<h1>Settings</h1>
 
-<select bind:value={$hyperlinkProvider}>
-	<option disabled> - English - </option>
-	<option value="https://quran.com/">Quran.com</option>
-	<option value="http://corpus.quran.com/wordbyword.jsp?chapter=">Quran Corpus - Ayah Analysis</option>
-	<option value="https://www.altafsir.com/Tafasir.asp?tMadhNo=1&tTafsirNo=86&tSoraNo=">Altafsir.org - Asbab al-Nuzul</option>
-	<option disabled> - Arabic - </option>
-	<option value="https://tafsir.app/">Tafsir.app</option>
-	<option value="https://www.altafsir.com/Tafasir.asp?tMadhNo=1&tTafsirNo=7&tSoraNo=">Altafsir.org - Ibn Katheer</option>
-</select>
+	<p>Choose your online reference provider:</p>
+
+	<select bind:value={$hyperlinkProvider}>
+		<option disabled> - English - </option>
+		<option value="https://quran.com/">Quran.com</option>
+		<option value="http://corpus.quran.com/wordbyword.jsp?chapter=">Quran Corpus - Ayah Analysis</option>
+		<option value="https://www.altafsir.com/Tafasir.asp?tMadhNo=1&tTafsirNo=86&tSoraNo=">Altafsir.org - Asbab al-Nuzul</option>
+		<option disabled> - Arabic - </option>
+		<option value="https://tafsir.app/">Tafsir.app</option>
+		<option value="https://www.altafsir.com/Tafasir.asp?tMadhNo=1&tTafsirNo=7&tSoraNo=">Altafsir.org - Ibn Katheer</option>
+	</select>
+
+	<br><br><br><br>
+	<p ><a href="#/" id="backLink">Back to Quran</a></p>
+
+</div>
 
 
-
-<p><a href="#/">Back to Quran</a></p>
