6d490f2e9a0ceb6bc5d6dfcdf09619d7048e62ad
Naved Islam <navedcoded@gmail.com>
2021-02-04 17:16:50 +0500

    attempt to get window icon for Linux

--- src/electron.js
+++ src/electron.js
@@ -28,7 +28,7 @@ function createMainWindow(){
 			preload: __dirname + '/preload.js'
 		},
 		title: app.name,
-  		icon: __dirname + '/icon.png',
+		icon: path.join(__dirname, '/icon/Icon-512x512.png'),
 		titleBarStyle: 'hidden',
         width: windowState.bounds && windowState.bounds.width || 1600,
 		height: windowState.bounds && windowState.bounds.height || 920,
